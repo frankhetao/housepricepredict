3d9cbef5835f068c9f507a47628aaf934eefe56d
frankhetao <hot20031103@163.com>
2017-03-31 16:30:11 +0800

    dd

---
+++ src/execute/HousePricePre.py
@@ -0,0 +1,118 @@
+#coding: utf-8
+from numpy import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import Series, DataFrame
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from keras.models import Sequential
+from keras.layers import Dense
+
+class HousePricePre:
+    def __init__(self,data):
+        self.data=data
+
+        # Function to show the resutls of linear fit model
+    def show_linear_line(self,X_parameters,Y_parameters):
+         # Create linear regression object
+         regr = self.getModel()
+         regr.fit(X_parameters, Y_parameters)
+         plt.scatter(X_parameters,Y_parameters,color='blue')
+         plt.plot(X_parameters,regr.predict(X_parameters),color='red',linewidth=4)
+         plt.xticks(())
+         plt.yticks(())
+         plt.show()
+         
+    def getModel(self):
+        return LinearRegression()
+    
+    def getKerasLinearModel(self):
+        model = Sequential()
+        input_dim_size=self.getXList().__len__()
+        model.add(Dense(input_dim=input_dim_size, output_dim=1, init='uniform', activation='linear'))
+        model.compile(optimizer='adam', loss='mse')
+        return model
+
+    def getXList(self):
+        #['hospital','bus','mall','subway','school','officeBuild','room_type','size','region','chaoxiang','builtdate']
+        #return ['builtdate','louchentype','loucheng','taxfree','size','hospital','bus','mall','subway','school','officeBuild','region_avg_price']
+        return ['builtdate','size','hospital','bus','mall','subway','school','officeBuild','region_avg_price']
+        #return ['size']
+    
+    def gety(self):
+        return 'unit_price';
+    
+    def kerasLinearFit(self,model):
+        data=self.data
+        X = data[self.getXList()].values#转换为numpy.ndarray
+        
+        y = data[self.gety()].values;#转换为numpy.ndarray
+        
+        #X = np.linspace(-1, 1, 101)
+        #y = 2 * X + np.random.randn(*X.shape) * 0.33 # create a y value which is approximately linear but with some random noise
+
+        model.fit(X, y, nb_epoch=50, verbose=1)
+        
+        weights = model.layers[0].get_weights()
+        w = weights[0][0][0]
+        b = weights[1][0]
+
+    def regress(self,model):
+        data=self.data
+        X = data[self.getXList()]
+        
+        y = data[self.gety()];
+        
+        linreg = model
+        
+        linreg.fit(X, y)
+        
+        y_pred=linreg.predict(X)
+        
+        print(linreg.intercept_)
+        print(linreg.coef_)
+        print(zip(self.getXList(), linreg.coef_))
+        
+        print('训练的结果如下：')
+        #误差评估
+        # calculate MAE using scikit-learn
+        print("MAE:",metrics.mean_absolute_error(y,y_pred))
+        # calculate MSE using scikit-learn
+        print("MSE:",metrics.mean_squared_error(y,y_pred))
+        # calculate RMSE using scikit-learn
+        print("RMSE:",np.sqrt(metrics.mean_squared_error(y,y_pred)))
+        
+        from sklearn.cross_validation import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+        linreg.fit(X_train, y_train)
+        
+        print(linreg.intercept_)
+        print(linreg.coef_)
+        print(zip(self.getXList(), linreg.coef_))
+        
+        y_pred2 = linreg.predict(X_test)
+        print('测试的结果如下：')
+                # calculate MAE using scikit-learn
+        print("MAE:",metrics.mean_absolute_error(y_test,y_pred2))
+        # calculate MSE using scikit-learn
+        print("MSE:",metrics.mean_squared_error(y_test,y_pred2))
+        # calculate RMSE using scikit-learn
+        print("RMSE:",np.sqrt(metrics.mean_squared_error(y_test,y_pred2)))
+        '''
+        plt.figure()
+        plt.plot(range(len(y_pred)),y_pred,'b',label="predict") 
+        plt.plot(range(len(y_pred)),y_test,'r',label="test")  
+        plt.legend(loc="upper right") #显示图中的标签  
+        plt.xlabel("the number of price_union")  
+        plt.ylabel('value of price_union')  
+        plt.show()
+        
+        import seaborn as sns
+        # visualize the relationship between the features and the response using scatterplots  
+        sns.pairplot(data, x_vars=['room_type','size','region','chaoxiang','builtdate'], y_vars='price_union', size=7, aspect=0.8)
+        plt.show()
+        
+        sns.pairplot(data, x_vars=['room_type','size','region','chaoxiang','builtdate'], y_vars='price_union', size=7, aspect=0.8, kind='reg')
+        plt.show()
+        '''
--- src/execute/PreProcess.py
+++ src/execute/PreProcess.py
@@ -4,15 +4,35 @@ import re
 
 class PreProcess:
     '''
-            预 处理需要分析的数据
+    预 处理需要分析的数据
     '''
 
-    def __init__(self, csvFile):
+    def __init__(self, csvFile,courtFeature,regionFile):
         self.csvFile=csvFile
+        self.courtFeature=courtFeature
+        self.regionFile=regionFile
         
     def process(self):
         data = pd.read_csv(self.csvFile, error_bad_lines=False,index_col=0)#为空的设置为NaN
+        #del data['builtdate']
         data=data.dropna(how='any')#去掉为NaN的那些行
         data.builtdate=data.builtdate.str.replace('年建', '')#将年份多余的汉字去掉
+        #data.loucheng=data.loucheng.str.replace('层', '')#将多余的汉字去掉
+        #data.taxfree=data.taxfree.str.replace('全税','0')
+        #data.taxfree=data.taxfree.str.replace('满五','5')
+        #data.taxfree=data.taxfree.str.replace('满二','2')
+        #data.louchentype=data.louchentype.str.replace('高区','3')
+        #data.louchentype=data.louchentype.str.replace('中区','2')
+        #data.louchentype=data.louchentype.str.replace('低区','1')
+        #data=data[data.name=='潍坊四村'] #过滤
+        #data.unit_price=data.unit_price/1000#精确到元，MSE太大，因此预测精确到千元即可，例如：一般讨论房子价格，都是单价1.5万元
+        data=data[1:]#去掉序号列
+        
+        dataFeature = pd.read_csv(self.courtFeature, error_bad_lines=False,index_col=0)#为空的设置为NaN
+        dataFeature=dataFeature.dropna(how='any')#去掉为NaN的那些行
+        data=pd.merge(dataFeature,data,on=['name'])
+        
+        dataRegion = pd.read_csv(self.regionFile, error_bad_lines=False,index_col=0)#为空的设置为NaN
+        data=pd.merge(data,dataRegion,on=['region'])
         return data
         
